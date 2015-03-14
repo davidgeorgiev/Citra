ff5d7ec598d676e12ee5af80cbc194e7666cbf9d
davidgeorgiev <david_georgiev@mail.bg>
2015-03-14 15:17:22 +0200

    Edited read me

---
+++ wait.rb
@@ -0,0 +1,7 @@
+puts "\e[H\e[2J"
+puts "========================================================"
+puts "  Now go and edit your edit_me.citra_config_file_23987  "
+puts "========================================================"
+puts "When you are ready press any key"
+puts "========================================================"
+gets.chomp
