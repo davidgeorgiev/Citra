1e2e2e4c8110c08329bec5389c25713af3285427
davidgeorgiev <david_georgiev@mail.bg>
2015-03-15 13:15:12 +0200

    Editing returning url to user

--- wait.rb
+++ wait.rb
@@ -1,6 +1,8 @@
 puts "\e[H\e[2J"
 puts "========================================================"
 puts "  Now go and edit your edit_me.citra_config_file_23987  "
+puts "  Don't change the address if you aren't running this   "
+puts "                  for first time!!!"
 puts "========================================================"
 puts "When you are ready press any key"
 puts "========================================================"
